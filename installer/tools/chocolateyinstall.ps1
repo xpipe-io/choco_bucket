7f304fa91c3fb16c22e33964b72f5d14b6094249
crschnick <crschnick@xpipe.io>
2023-06-01 11:19:07 +0000

    1.1.0

--- installer/tools/chocolateyinstall.ps1
+++ installer/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
-# Generated with JReleaser 1.6.0 at 2023-05-24T08:43:18.699004113Z
+# Generated with JReleaser 1.6.0 at 2023-06-01T11:18:46.55576874Z
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = 'xpipe'
   fileType      = 'msi'
-  url           = 'https://github.com/xpipe-io/xpipe/releases/download/1.0.1/xpipe-installer-windows-x86_64.msi'
+  url           = 'https://github.com/xpipe-io/xpipe/releases/download/1.1.0/xpipe-installer-windows-x86_64.msi'
   silentArgs    = "/quiet"
   validExitCodes= @(0)
   softwareName  = 'xpipe*'
-  checksum      = '3b55761dc204981056c263c2f7f648ea20e60456b713d0e73bc6d94c3c894775'
+  checksum      = '9727b6d167d71e6a748392345df95bf83aaaed773ee1a6c1fe8e8dbbb85a05ed'
   checksumType  = 'sha256'
 }
 
