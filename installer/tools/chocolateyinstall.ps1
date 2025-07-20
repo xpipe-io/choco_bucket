980b82020a3a9a68b2ed99d9e862e4a2aff550d0
Christopher Schnick <crschnick@xpipe.io>
2025-07-20 05:47:04 +0000

    17.1

--- installer/tools/chocolateyinstall.ps1
+++ installer/tools/chocolateyinstall.ps1
@@ -1,17 +1,13 @@
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-$RawArch = [System.Runtime.InteropServices.RuntimeInformation,mscorlib]::OSArchitecture.ToString().ToLower();
-$Arch = If ($RawArch -eq "x64") {"x86_64"} Else {"arm64"}
-$Checksum = If ($RawArch -eq "x64") {"991b351d9a0574e455541d1a357432cc68385c22ff536e35fe9e10f1badbce40"} Else {"66767e3fb6669c625cb5e7a17aa10efe3a54692696e356d2c7e2e571b4c73dad"}
-
 $packageArgs = @{
   packageName   = 'xpipe'
   fileType      = 'msi'
-  url           = "https://github.com/xpipe-io/xpipe/releases/download/17.0/xpipe-installer-windows-$Arch.msi"
+  url           = 'https://github.com/xpipe-io/xpipe/releases/download/17.1/xpipe-installer-windows-x86_64.msi'
   silentArgs    = "/quiet"
   validExitCodes= @(0)
   softwareName  = 'xpipe*'
-  checksum      = $Checksum
+  checksum      = '768ef0ab42dc4a7867bf6cb7b52dc09534804c654ee5cb7f79c7320fc8a88f71'
   checksumType  = 'sha256'
 }
 
