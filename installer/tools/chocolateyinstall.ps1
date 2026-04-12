b89d2745db72430d80f453b5458174a6cf8cf35b
Christopher Schnick <crschnick@xpipe.io>
2026-04-12 11:35:54 +0000

    22.9

--- installer/tools/chocolateyinstall.ps1
+++ installer/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@ $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = 'xpipe'
   fileType      = 'msi'
-  url           = 'https://github.com/xpipe-io/xpipe/releases/download/22.8/xpipe-installer-windows-x86_64.msi'
+  url           = 'https://github.com/xpipe-io/xpipe/releases/download/22.9/xpipe-installer-windows-x86_64.msi'
   silentArgs    = "/quiet"
   validExitCodes= @(0)
   softwareName  = 'xpipe*'
-  checksum      = '1069baef62d72edbdd0e59a19f83586f3ed5273b3cc7823eb25a2c6991ff749f'
+  checksum      = '3ba079d832cdd204858f1e1038e9d2336baa4d4215f168a72a8c3b1d423dbb94'
   checksumType  = 'sha256'
 }
 
